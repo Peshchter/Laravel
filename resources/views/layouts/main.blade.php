d6f32aadb8c6543d500290adee2b2caa1bc9f9d7
Pavel <leniviy.putnik@gmail.com>
2020-01-30 20:28:39 +0400

    little edit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
     <title>Document</title>
 </head>
 <body>
