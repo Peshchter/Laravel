158df76dbf7203f8d0bcc80fe095070b53fcd62c
Pavel <leniviy.putnik@gmail.com>
2020-02-18 22:25:09 +0400

    Исправлены вьюхи для всех страниц под общий стиль

--- resources/views/News/newsOne.blade.php
+++ resources/views/News/newsOne.blade.php
@@ -1,9 +1,23 @@
-
-@extends('layouts.appn')
+@extends('layouts.app')
 @section('content')
-    <h1>{{$model['title']}}</h1>
-    <h2>Категория: <a href="{{route('news.categories.byId', $category['id'])}}">{{$category['title']}}</a> </h2>
-    <p>{{$model['text']}} </p>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h1>{{$model['title']}}</h1>
+                    </div>
 
-
+                    <div class="card-header">
+                        <h3>Категория:
+                            <a href="{{route('news.categories.byId', $category['id'])}}">{{$category['title']}}</a>
+                        </h3>
+                    </div>
+                    <div class="card-body">
+                        <p>{{$model['text']}} </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                Главная страница.
+                <div class="card">
+                    <div class="card-header">Главная страница</div>
+                </div>
             </div>
         </div>
     </div>
